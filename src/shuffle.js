40419173a0f2a5294f46622874c666c935b903b1
Nolan Eckert <nolan.eckert@gmail.com>
2021-05-05 14:01:15 -0600

    Add fisher-yates shuffle code and documentation comments to shuffle.js

--- src/shuffle.js
+++ src/shuffle.js
@@ -8,4 +8,24 @@
  * @returns {array} - Mutated input data array.
  */
 
-const shuffle = (arr) => {};
+const shuffle = (arr) => {
+  let currIndex = arr.length;
+  let rdmIndex;
+  let tempValue;
+
+  // While currIndex is NOT index 0
+  while (currIndex) {
+    // Assign a random index to use the element to swap
+    rdmIndex = Math.floor(Math.random() * currIndex);
+    currIndex--;
+
+    // Swap two values by:
+    // Store the value at current index so as not to lose it
+    tempValue = arr[currIndex];
+    // Set the current index value to the value at the random index
+    arr[currIndex] = arr[rdmIndex];
+    // Set the random index value to the initial value of the current index
+    arr[rdmIndex] = tempValue;
+  }
+  return arr;
+};
