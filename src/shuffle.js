8a28940a060bbc3c16347761f10e29da1195d4d0
Nolan Eckert <nolan.eckert@gmail.com>
2021-05-05 13:45:09 -0600

    Add src folder and create shuffle.js

---
+++ src/shuffle.js
@@ -0,0 +1,11 @@
+/**
+ * Returns the original array after mutation.
+ * Shuffle function uses the Fisher-Yates Shuffle Modern algorithm to take an array and randomly shuffle its data.
+ * The algorithm starts with the final index, generates a random index, then swaps the data at the final index with the random one.
+ * It continues to do this, working its way from final index to first index where it ends.
+ *
+ * @param {array} arr - The input data array.
+ * @returns {array} - Mutated input data array.
+ */
+
+const shuffle = (arr) => {};
